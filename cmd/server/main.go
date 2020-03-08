060ac397a3abe6b2a6ade3ba8b7e3fe28b41bc8d
Crypta Eve <crypta@crypta.tech>
2020-03-08 13:52:18 +1100

    fixed memcached address

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	svr, err := server.NewServer("PoddedBouncer - Crypta Electrica", "127.0.0.1:111")
+	svr, err := server.NewServer("PoddedBouncer - Crypta Electrica", "127.0.0.1:11211")
 	if err != nil {
 		log.Fatalln(err)
 	}
